a0cdf58f026d1a2369ed5458597460be6c1b232a
Yuuk1A <zm18390@josai.ac.jp>
2020-11-17 02:04:01 +0000

    1117

--- ex6-5.js
+++ ex6-5.js
@@ -1,3 +1,9 @@
-var who ='world';
+var who65 ='world';
+function sayhello65(){
+alert('Hello,'+ who65 + '!');}
+function taro65(){who='太郎';}
+function hanako(){who='花子';}
+
+
 
 
--- myjs.js
+++ myjs.js
@@ -1,4 +1,3 @@
 //alert('Hello, Yuuki!');
 alert(1+1);
 alert(Math.sqrt(1+1));
-
