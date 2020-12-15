c8310a3738178296007be10ac8e5a6719ce7ee5c
Yuuk1A <zm18390@josai.ac.jp>
2020-12-15 07:18:36 +0000

    1215．PM16

--- ex6-6.js
+++ ex6-6.js
@@ -1,2 +1,8 @@
 function sayhello66(){var who66 ='world';
-alert('Hello,'+ who66 + '!');}
+alert('Hello,'+ who66 + '!');} 　　　　　　 //この構成でsayhello66はボタンを押せば起動
+                                           //つまり、taro,hanakoも同じようにするためには
+function taro66(){var gakita66 ='が来た';　//「who66」の代わりに新しく「gakita66」を使っ（登録し）て
+alert('太郎' + gakita66 + '！');}          //同じようにコードを入力すれば　ボタンを押せば起動する
+　　　　　　　　　　　　　　　　　　　　　　　// varは「登録」　alertは「～をよび出す的な感じ」
+function hanako66(){var gakita66='が来た';
+alert('花子'　+ gakita66 + '！');}
