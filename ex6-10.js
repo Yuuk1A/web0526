c6655c80c613f0bab80720041e6b53894850a5e7
Yuuk1A <zm18390@josai.ac.jp>
2021-01-12 02:10:17 +0000

    １・１２

--- ex6-10.js
+++ ex6-10.js
@@ -1,3 +1,4 @@
 function imgchange1(){document.getElementById('logo').src = './image/JosaiLogo.png';}
-//index.html に入力してあるjosai.pngをJosaiLogo.pngに変えるためのコード
+//1.index.html に入力してあるjosai.pngをJosaiLogo.pngに変えるためのコード
 function imgchange2(){document.getElementById('logo').src = './image/josai.png';}
+//2. 1の逆バージョン
