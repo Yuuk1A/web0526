b67a56020e3ade19ef7656e0a8a99abf2db58198
Yuuk1A <zm18390@josai.ac.jp>
2021-01-30 15:00:26 +0000

    131

--- ex6-11.js
+++ ex6-11.js
@@ -1,4 +1,7 @@
 function imgchange3(){document.getElementById('do').src = './image/dow.png';}
 function imgchange4(){document.getElementById('do').src = './image/dop.png';}
 
-  
+function imgchange() { alert("3秒経ちました！"); }
+// ex6-9.5のコードと ex6-10のコードを参考にする
+//ex6-10ではmes ここではimgchange4にしたいから（imgchange4,3000)
+//ミリ単位のため　1000=1秒
---
+++ ex6-12.js
@@ -0,0 +1,4 @@
+function imgchange5(){document.getElementById('yu').src = './image/yuki.png';}
+function imgchange6(){document.getElementById('yu').src = './image/yukitoke.png';}
+ 
+function image(){ alert("3秒経ちました！"); }
